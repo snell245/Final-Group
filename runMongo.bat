958255caca9e20565fb9a6f80d2ba761fd326a75
Joseph Jennings <jjennings@student.neumont.edu>
2018-12-06 16:17:08 -0700

    Fixes
    I fixed the mongo issue.

--- runMongo.bat
+++ runMongo.bat
@@ -2,3 +2,5 @@
 Multi-line comment 1
 FILE PATH TO MONGO --dbpath FILE PATH TO FILE TO STORE/EXECUTE MONGO
 :endComment
+
+"C:\Program Files\MongoDB\Server\3.6\mongod" --dbpath "C:\Users\Sasuke-Joseph\Documents\GitHub\Final-Group\data"
